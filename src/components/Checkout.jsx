e72d45b5c85959daf64992de83935d3941ab4f26
Irene Toxla <toxlairene@gmail.com>
2025-07-16 13:05:05 +0000

    updated checkout component

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -42,7 +42,7 @@ const Checkout = () => {
   return (
     <>
       <div className="w-full h-[30rem] bg-green-300 flex items-center">
-        <div className="flex flex-col items-center gap-10  bg-green-500 rounded-xl shadow-md lg:w-[50%] max-w-sm h-[80%] mx-auto  transition-transform duration-300 hover:-translate-y-2">
+        <div className="flex flex-col items-center gap-10  bg-green-500 rounded-xl shadow-md lg:w-[50%] max-w-sm h-[80%] mx-auto  transition-transform duration-300 hover:-translate-y-2 px-4">
           <h2 className="pt-6 text-white text-2xl font-bold">Checkout</h2>
           <div className="flex flex-col ">
             <label htmlFor="" className="text-white">
@@ -50,7 +50,7 @@ const Checkout = () => {
             </label>
             <input
               type="email"
-              className="bg-white px-6 py-2 rounded "
+              className="bg-white px-6 py-2 rounded"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
